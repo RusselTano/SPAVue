671918ada6fe77779efff13c86c2c4fe6e9b3721
Dylane <tanolamene@gmail.com>
2024-09-29 12:51:07 -0300

    Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <a href="#" class="header__logo">Mon site</a>
+    <nav>
+      <ul>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#contact"></a>Contact</li>
+        <li><a href="#"></a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  background-color: #13171f;
+  border-bottom: 1px solid #fefefe;
+
+  &__logo {
+    font-size: 2px;
+  }
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
